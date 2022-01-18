c6ea2d26fe111eaa3080bfb42fd5f9f2d29be2e6
eid-aboelmajd <eng.eid.aboelmajd@gmail.com>
2022-01-18 10:44:41 +0300

    adding remaining actions

--- Routers/noteRouter.js
+++ Routers/noteRouter.js
@@ -5,6 +5,10 @@ const noteController = require("../Controllers/noteController");
 const router = express.Router();
 
 router.get("/notes" ,noteController.Api.getAllNotes);
+router.get("/notes/note" ,noteController.Api.getNote);
+router.post("/notes/createnote" ,noteController.Api.createNote);
+router.put("/notes/updatenote" ,noteController.Api.updateNote);
+router.delete("/notes/deletenote" ,noteController.Api.deleteNote);
 
 module.exports = router;
 
